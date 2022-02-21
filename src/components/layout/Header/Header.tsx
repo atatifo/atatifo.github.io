bd6a4b9b14a474d3124873c9b01bd085794f2899
Ali Ataf <aliattaf98@gmail.com>
2022-02-21 22:46:23 +0400

    feat: edit about page

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -9,8 +9,8 @@ import UnstyledLink from '@/components/links/UnstyledLink';
 const links = [
   { href: '/about', label: 'About' },
   { href: '/projects', label: 'Projects' },
-  { href: '/contact', label: 'Contact' },
-  { href: '/blog', label: 'Blog' },
+  /*   { href: '/contact', label: 'Contact' },
+  { href: '/blog', label: 'Blog' }, */
 ];
 
 export default function Header() {
@@ -19,11 +19,14 @@ export default function Header() {
   return (
     <header className='bg-primary sticky top-0 z-50 drop-shadow-lg'>
       <div className='layout flex justify-between items-center pb-1 h-16'>
-        <UnstyledLink href='/' className='font-bold'>
+        <UnstyledLink
+          href='/'
+          className='font-bold transition-all hover:text-action'
+        >
           &lt;Ali Ataf /&gt;
         </UnstyledLink>
         <nav>
-          <ul className='flex justify-between items-center space-x-5 h-16'>
+          <ul className='flex justify-between items-center space-x-2 h-16 sm:space-x-5'>
             {links.map(({ href, label }) => (
               <UnstyledLink href={href} key={`${href}${label}`}>
                 <li
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,42 +1,30 @@
-import { GetStaticProps, GetStaticPropsContext } from 'next';
-
 import Button from '@/components/buttons/Button';
 import BgCircle from '@/components/layout/BgCircle';
 import Seo from '@/components/Seo';
 
-export const asyncgetStaticProps: GetStaticProps = async (
-  context: GetStaticPropsContext
-) => {
+export function getStaticProps() {
   return {
-    props: {
-      title: context,
-    },
+    props: {},
   };
-};
+}
 
-type Props = {
-  title: string;
-};
-
-export default function HomePage({ title }: Props) {
+export default function HomePage() {
   return (
     <>
       <Seo templateTitle='About' />
 
       <section className='bg-secondary'>
-        <div className='flex justify-between min-h-screen'>
+        <div className='block justify-between min-h-screen sm:flex'>
           <BgCircle width='45vw' height='45vw' animate doubleBorder />
-          <div className='flex-1 pt-32'>
-            <div className='flex items-center'>
+          <div className='flex-1 p-4 sm:pt-32'>
+            <div className='flex items-center mt-6'>
               <div className='pr-6 text-3xl'>About Me</div>
               <div
                 className='flex-1 border border-yellow-400'
                 style={{ height: '1px' }}
-              >
-                {title}
-              </div>
+              ></div>
             </div>
-            <div className='pr-16 mt-16 leading-10 text-gray-300'>
+            <div className='mt-6 leading-10 text-gray-300 sm:pr-16 sm:mt-16'>
               I am a fresh graduated Software Engineer with 2+ years of
               experience in web development. I have a passion for building
               scalable and maintainable web applications. I am currently working
@@ -50,6 +38,19 @@ export default function HomePage({ title }: Props) {
                 Nestech
               </a>
               .
+              <br />
+              Actually, I started to code in 2013 (~15 years old), with Visual
+              Basic 6.0 and I built a lot of programs and games in
+              it(Unfortunately, I didn&apos;t upload them on any public
+              repository 😢).
+              <br />
+              Then, I started to learn JavaScript and I have been using it since
+              then. I have been working with texhnologies like{' '}
+              <span className='text-gray-600'>
+                {' '}
+                React, Next.js, Vue.js, Typescript, Tailwindcss, Sass, and many
+                more.
+              </span>
             </div>
 
             <div className='mt-16'>
